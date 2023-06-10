9f652c779bdfae18b4bea950039fec1bfd57007b
Do Tien Dat <112066833+Datdo3108@users.noreply.github.com>
2023-06-10 11:07:10 +0700

    Add files via upload

---
+++ Multi ESP sender 1.ino
@@ -0,0 +1,91 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp-now-many-to-one-esp8266-nodemcu/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <ESP8266WiFi.h>
+#include <espnow.h>
+#include <Wire.h>
+#include <MPU6050_light.h>
+
+MPU6050 mpu(Wire);
+
+// REPLACE WITH RECEIVER MAC Address
+uint8_t broadcastAddress[] = {0x10, 0x52, 0x1C, 0xE2, 0xE1, 0x50};
+
+// Set your Board ID (ESP32 Sender #1 = BOARD_ID 1, ESP32 Sender #2 = BOARD_ID 2, etc)
+#define BOARD_ID 1
+
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message {
+    int id;
+    float x;
+    float y;
+    float z;
+} struct_message;
+
+// Create a struct_message called test to store variables to be sent
+struct_message myData;
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 1;
+
+// Callback when data is sent
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
+  Serial.print("\r\nLast Packet Send Status: ");
+  if (sendStatus == 0){
+    Serial.println("Delivery success");
+  }
+  else{
+    Serial.println("Delivery fail");
+  }
+}
+ 
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+  Wire.begin();
+  mpu.begin();
+ 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+
+  // Init ESP-NOW
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  } 
+  // Set ESP-NOW role
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+
+  // Once ESPNow is successfully init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+
+}
+ 
+void loop() {
+  mpu.update();
+  if ((millis() - lastTime) > timerDelay) {
+    // Set values to send
+    myData.id = BOARD_ID;
+    myData.x = mpu.getAngleX();
+    myData.y = mpu.getAngleY();
+    myData.z = mpu.getAngleZ();
+
+    // Send message via ESP-NOW
+    esp_now_send(0, (uint8_t *) &myData, sizeof(myData));
+    lastTime = millis();
+  }
+}
